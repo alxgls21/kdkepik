123b1ff864013e55a2d88dde8c489b21ae542f7b
kill21er <63592075+alxgls21@users.noreply.github.com>
2024-08-06 14:21:27 +0300

    Remove ipiresies.html and related code

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('ypersyndesmos/', views.ypersyndesmos, name='ypersyndesmos'),
+    path('ypersyndesmos/', views.ypersyndesmos_view, name='ypersyndesmos'),
     path('anafora_aks_ipiresias/', views.anafora_aks_ipiresias, name='anafora_aks_ipiresias'),
+    path('epilochias/', views.epilochias_view, name='epilochias'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -4,15 +4,27 @@ from .models import DidesCategory, HarpCategory, AdmeCategory
 def index(request):
     return render(request, 'index.html')
 
-def ypersyndesmos(request):
-    dides_categories = DidesCategory.objects.all()
+def ypersyndesmos_view(request):
+    unclassified_network = DidesCategory.objects.filter(category='unclassified')
+    classified_network = DidesCategory.objects.filter(category='classified')
     harp_categories = HarpCategory.objects.all()
     adme_categories = AdmeCategory.objects.all()
-    return render(request, 'ypersyndesmos.html', {
-        'dides_categories': dides_categories,
+    
+    context = {
+        'unclassified_network': unclassified_network,
+        'classified_network': classified_network,
         'harp_categories': harp_categories,
-        'adme_categories': adme_categories
-    })
+        'adme_categories': adme_categories,
+    }
+    
+    return render(request, 'ypersyndesmos.html', context)
 
 def anafora_aks_ipiresias(request):
     return render(request, 'anafora_aks_ipiresias.html')
+
+def epilochias_view(request):
+    return render(request, 'epilochias.html')
+
+def ipiresies_view(request):
+    services = Service.objects.all()
+    return render(request, 'ipiresies.html', {'services': services})
