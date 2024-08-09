ae3d8cb48315259f28034a97c72dce743ab6923c
kill21er <alexgoulas21@gmail.com>
2024-08-09 13:56:50 +0300

    Προσθήκη κουμπιού για υποβολή και λήψη της φόρμας σε PDF

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,8 +7,4 @@ urlpatterns = [
     path('ypersyndesmos/', views.ypersyndesmos_view, name='ypersyndesmos'),
     path('anafora_aks_ipiresias/', views.anafora_aks_ipiresias, name='anafora_aks_ipiresias'),
     path('epilochias/', views.epilochias_view, name='epilochias'),
-    path('report/', views.report_view, name='report'),
-    path('download_pdf/<int:pk>/', views.download_pdf, name='download_pdf'),
-    path('success/', TemplateView.as_view(template_name='success.html'), name='success'),
-    path('anafora_ipiresia/', views.report_view, name='anafora_ipiresia'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,9 +1,6 @@
-from django.http import HttpResponse
 from django.template.loader import render_to_string
-from xhtml2pdf import pisa
 from django.shortcuts import render, redirect
-from .models import DidesCategory, HarpCategory, AdmeCategory, Report
-from .forms import ReportForm
+from .models import DidesCategory, HarpCategory, AdmeCategory
 
 def index(request):
     return render(request, 'index.html')
@@ -28,32 +25,3 @@ def anafora_aks_ipiresias(request):
 
 def epilochias_view(request):
     return render(request, 'epilochias.html')
-
-def ipiresies_view(request):
-    services = Service.objects.all()
-    return render(request, 'ipiresies.html', {'services': services})
-
-def report_view(request):
-    if request.method == 'POST':
-        form = ReportForm(request.POST)
-        if form.is_valid():
-            form.save()
-            # Παραμένουμε στην ίδια σελίδα και προσθέτουμε ένα μήνυμα επιτυχίας
-            return render(request, 'anafora_ipiresia.html', {'form': form, 'success': True})
-    else:
-        form = ReportForm()
-    return render(request, 'anafora_ipiresia.html', {'form': form})
-
-def render_to_pdf(template_src, context_dict={}):
-    template = render_to_string(template_src, context_dict)
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = 'attachment; filename="report.pdf"'
-    pisa_status = pisa.CreatePDF(template, dest=response)
-    if pisa_status.err:
-        return HttpResponse('We had some errors <pre>' + template + '</pre>')
-    return response
-
-def download_pdf(request, pk):
-    report = Report.objects.get(pk=pk)
-    context = {'report': report}
-    return render_to_pdf('anafora_ipiresia.html', context)
